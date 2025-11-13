508937d26cd8d84206a143cb3ce189eb05d437a6
ben@orion <128577990+bencoronard@users.noreply.github.com>
2025-11-13 15:07:12 +0700

    Init unit test files

---
+++ src/test/java/dev/hireben/url_shortener/auth/dto/LoginRequestBodyTests.java
@@ -0,0 +1,10 @@
+package dev.hireben.url_shortener.auth.dto;
+
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+final class LoginRequestBodyTests {
+
+  // -----------------------------------------------------------------------------
+
+}
---
+++ src/test/java/dev/hireben/url_shortener/url/service/UrlMappingServiceImplTests.java
@@ -0,0 +1,12 @@
+package dev.hireben.url_shortener.url.service;
+
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+final class UrlMappingServiceImplTests {
+
+  // =============================================================================
+
+  // -----------------------------------------------------------------------------
+
+}
